728d7b1b2fe46982888cd489418de988a92be835
Samstringz <83889735+samstringz@users.noreply.github.com>
2024-06-25 12:03:07 +0100

    index.js

--- tutorial/index.js
+++ tutorial/index.js
@@ -14,3 +14,12 @@ function save(){
 }
 
 save()
+
+// sends message to users 
+let username = "per"
+
+let message = "You have three new notifications"
+
+let messageToUser = message + "," + username
+
+console.log(messageToUser)
